4e0056871af0629a4686ddda05f3a1322d994275
Your Name <you@example.com>
2021-02-08 14:28:50 -0300

    first notifica

--- index.js
+++ index.js
@@ -40,6 +40,11 @@ app.get('/pagar', async (req, res)=> {
     
 })
 
+app.post('/not', (req, res)=> {
+    console.log(req.query)
+    res.send('Ok')
+})
+
 app.listen(3000, () => {
     console.log('Server running...')
 })
